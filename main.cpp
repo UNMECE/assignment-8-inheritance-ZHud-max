2ef4046e7d6e1fc627b09ae5d3217ae6b5b16252
ZHud-max <zebhudnall@gmail.com>
2025-11-05 12:05:23 -0700

    Added operator= to Field class

--- main.cpp
+++ main.cpp
@@ -20,9 +20,23 @@ class Field {
 																										 value[0] = vx;
 																										 value[1] = vy;
 																										 value[2] = vz;}
+
+
+		// Copy Operator
+		Field& operator=(const Field& copy) {
+
+			value = new double[3];
+
+			value[0] = copy.value[0];
+			value[1] = copy.value[1];
+			value[2] = copy.value[2];
+
+			return *this;
+
+		}
 		
 		// Deconstructor
-		//~Field() {delete[] value; cout << "Destructor called for Field()" << endl;}
+		~Field() {delete[] value; cout << "Destructor called for Field()" << endl;}
 
 		// Getters
     double getV_X() {return value[0];}
@@ -54,8 +68,6 @@ class Electric_Field : public Field {
 		// Deconstructor
 		~Electric_Field() {cout << "Destructor called for E_Field" << endl;}
 
-		double get_E() {return E;}
-
 		void calcElectricField(double r, double Q) {E = Q / (4 * M_PI * pow(r, 2) * 8.85e-12);}
 
 		Electric_Field operator+(const Electric_Field& eField) const {
@@ -70,7 +82,7 @@ class Electric_Field : public Field {
 		}
 		
 		friend ostream &operator<<(ostream &os, const Electric_Field &eField) {
-			os << "Field: " << eField.E << "\nX: " << eField.value[0] << "\nY: " <<  eField.value[1] << "\nZ: " << eField.value[2];
+			os << "Field:\nM: " << eField.E << "\nX: " << eField.value[0] << "\nY: " <<  eField.value[1] << "\nZ: " << eField.value[2];
 			return os;
 		}
 
@@ -91,8 +103,6 @@ class Magnetic_Field : public Field {
 		// Deconstructor
 		~Magnetic_Field() {cout << "Destructor called for M_Field" << endl;}
 
-		double get_B() {return B;}
-
 		void calcMagneticField(double r, double I) {B = I / (2 * M_PI * r * 1.26e-6);}
 		
 		Magnetic_Field operator+(const Magnetic_Field& mField) const {
@@ -107,7 +117,7 @@ class Magnetic_Field : public Field {
 		}
 		
 		friend ostream &operator<<(ostream &os, const Magnetic_Field &mField) {
-			os << "Field: " << mField.B << "\nX: " << mField.value[0] << "\nY: " <<  mField.value[1] << "\nZ: " << mField.value[2];
+			os << "Field:\nB: " << mField.B << "\nX: " << mField.value[0] << "\nY: " <<  mField.value[1] << "\nZ: " << mField.value[2];
 			return os;
 		}
 
@@ -116,10 +126,10 @@ class Magnetic_Field : public Field {
 
 int main() {
 
-	Electric_Field e1(1e5, 10.9, 1.7e2), e2(1e5, 10.9, 1.7e2), e3;
+	Electric_Field e1(1, 2, 3), e2(1, 1, 1), e3;
 	e3 = e1 + e2;
 
-	Magnetic_Field m1(1e5, 10.9, 1.7e2), m2(1e5, 10.9, 1.7e2), m3;
+	Magnetic_Field m1(1.3e4, 10.8, 1.5e3), m2(1e5, 10.9, 1.7e2), m3;
 	m3 = m1 + m2;
 
 	e1.calcElectricField(2, 5);
